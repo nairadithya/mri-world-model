6a800284e577edbee433c5ccb1a517fd30938543
Adithya Nair <adithyanair121@gmail.com>
2026-09-05 10:13:34 +0530

    make run_train path insert cwd-robust

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -13,7 +13,7 @@ import torch
 import yaml
 from torch.utils.data import DataLoader
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.data.collate import make_collate
 from src.data.dataset import LUMIEREDataset
 from src.data.splits import patient_splits
